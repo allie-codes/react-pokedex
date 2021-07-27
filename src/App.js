41dd491b2da4ece8aacff1dcb4c446c049d8ae0a
allie <alliemeowy@gmail.com>
2021-07-27 09:08:32 -0700

    Project Setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Pokedex from './Pokedex';
+
+const App = () => {
+
+  return (
+    <div>
+     <h1>Hello!</h1>
+     <Pokedex />
+    </div>
+  )
+}
+
+
+export default App;
---
+++ src/Pokedex.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Pokedex = () => {
+    return(
+        <div className="pokedex">
+            <p>Pikachu!</p>
+        </div>
+    );
+}
+
+export default Pokedex;
